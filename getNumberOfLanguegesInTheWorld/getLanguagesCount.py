6f90f73b43373b047224e5758ef5f748fd1f10ed
pusto-tauranth <619980817@qq.com>
2019-04-24 12:42:19 +0800

    Update getLanguagesCount.py

--- getNumberOfLanguegesInTheWorld/getLanguagesCount.py
+++ getNumberOfLanguegesInTheWorld/getLanguagesCount.py
@@ -1,5 +1,14 @@
 fname = 'languages.txt'
 cnt = 0
+for l in open(fname):
+    if l.find('(')==-1 : continue
+    while l[l.find('(')+1]<'0' or l[l.find('(')+1]>'9':
+        l = l[l.find(')')+1:]
+    st = l[l.find('(')+1 : l.find(')')]
+    if st[0]>='0' and st[0] <= '9': cnt+= int(st)
+print(cnt) # which is 7474
+"""
+# another read method
 with open(fname, 'r') as f:
     while True:
         l = f.readline()
@@ -9,4 +18,4 @@ with open(fname, 'r') as f:
             l = l[l.find(')')+1:]
         st = l[l.find('(')+1 : l.find(')')]
         if st[0]>='0' and st[0] <= '9': cnt+= int(st)
-print(cnt) # which is 7474
+"""
